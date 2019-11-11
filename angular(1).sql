14fd11fcd4f81e21c40a537c95537446d68cf3ae
Nasarfa <57620547+Nasarfa@users.noreply.github.com>
2019-11-11 16:52:52 +0530

    Update angular(1).sql

--- angular(1).sql
+++ angular(1).sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
 -- version 4.7.4
--- https://www.phpmyadmin.net/
+-- https://www.phpmyadmin.ne
 --
 -- Host: 127.0.0.1:3306
 -- Generation Time: Sep 18, 2018 at 06:06 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZER
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `customer` (
 ) ENGINE=MyISAM AUTO_INCREMENT=110 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `customer`
+-- Dumping data for table `custome
 --
 
 INSERT INTO `customer` (`id`, `name`, `mobile`, `email`, `address`, `customer_image`) VALUES
